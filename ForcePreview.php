be035341aae23e6fd94f6aabde9975acce50503b
didicodes <edidiongasikpo@gmail.com>
2019-04-09 02:07:32 +0100

    Change HTTP links to mediawiki.org to HTTPS
    Bug: T189687
    Change-Id: I7357212290e90d9e46e07d639c32455352bde03c

--- ForcePreview.php
+++ ForcePreview.php
@@ -6,7 +6,7 @@
  * @ingroup Extensions
  * @author Ryan Schmidt
  * @license MIT
- * @link http://www.mediawiki.org/wiki/Extension:ForcePreview Documentation
+ * @link https://www.mediawiki.org/wiki/Extension:ForcePreview Documentation
  */
 
 class ForcePreview {
