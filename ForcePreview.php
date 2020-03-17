28f358f79dbe216b5c6b318f0ac4a8e099ab21de
DannyS712 <DannyS712.enwiki@gmail.com>
2020-03-17 20:09:59 +0000

    Remove use of Title::userCan in MW 1.33+
    Bug: T246139
    Change-Id: Ifc5912e372690e9524a2a68d32f558af42fe0209

--- ForcePreview.php
+++ ForcePreview.php
@@ -9,6 +9,8 @@
  * @link https://www.mediawiki.org/wiki/Extension:ForcePreview Documentation
  */
 
+use MediaWiki\MediaWikiServices;
+
 class ForcePreview {
 	public static function onEditPageBeforeEditButtons( $editpage, &$buttons, &$tabindex ) {
 		$user = $editpage->getContext()->getUser();
@@ -27,15 +29,27 @@ class ForcePreview {
 	public static function onBeforePageDisplay( &$out, &$skin ) {
 		$user = $out->getUser();
 		$request = $out->getRequest();
-		$title = $out->getTitle();
 
 		if (
-			!$title->userCan( 'edit' )
-			|| $user->isAllowed( 'forcepreviewexempt' )
+			$user->isAllowed( 'forcepreviewexempt' )
 			|| !$user->getBoolOption( 'uselivepreview' )
 			|| !in_array( $request->getVal( 'action' ), [ 'edit', 'submit' ] )
 		) {
 			return true;
+		}
+
+		$title = $out->getTitle();
+		if ( class_exists( 'MediaWiki\Permissions\PermissionManager' ) ) {
+			if ( !MediaWikiServices::getInstance()
+				->getPermissionManager()
+				->userCan( 'edit', $user, $title )
+			) {
+				return true;
+			}
+		} else {
+			if ( !$title->userCan( 'edit' ) ) {
+				return true;
+			}
 		}
 
 		$out->addModules( 'ext.ForcePreview.livePreview' );
@@ -43,7 +57,7 @@ class ForcePreview {
 	}
 
 	public static function onResourceLoaderGetConfigVars( &$vars ) {
-		$config = MediaWiki\MediaWikiServices::getInstance()->getMainConfig();
+		$config = MediaWikiServices::getInstance()->getMainConfig();
 		$vars['wgEditSubmitButtonLabelPublish'] = $config->get( 'EditSubmitButtonLabelPublish' );
 		return true;
 	}
